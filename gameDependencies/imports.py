9bdcdae33fd0fb84a717c998389988ad7f12747f
Pablo González <holtermorter99@gmail.com>
2022-11-07 22:59:28 +0100

    imports

--- gameDependencies/imports.py
+++ gameDependencies/imports.py
@@ -1,3 +1,7 @@
-
-
+from objects.objectsclss.entityclss import *
+from objects.objectsclss.foodsclss import *
+from objects.objectsclss.inventoryclss import *
+from objects.objectsclss.swordclss import *
+from objects.objectsdec.entitydec import *
+from objects.objectsdec.swordsdec import *
 
